a024f64cfe3ee301c49ddeab2b7a0de4f8aa8133
tioprayudha <tioprayudha07@gmail.com>
2024-08-27 15:19:49 +0700

    Create Footer components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
 import TitleCards from "../../components/TitleCards/TitleCards";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -38,6 +39,7 @@ const Home = () => {
         <TitleCards title={"Upcoming"} />
         <TitleCards title={"Top pics for you"} />
       </div>
+      <Footer />
     </div>
   );
 };
