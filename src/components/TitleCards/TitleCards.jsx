48a7ab708e1fda6bba365cad9fc6350aab079ad6
tioprayudha <tioprayudha07@gmail.com>
2024-08-28 12:14:23 +0700

    Fetching API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/prop-types */
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const API = import.meta.env.VITE_API_AUTHORIZATION;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: API,
+    },
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -13,17 +23,29 @@ const TitleCards = ({ title, category }) => {
 
   useEffect(() => {
     cardsRef.current.addEventListener("wheel", handleWheel);
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="card image" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt="card image"
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -34,10 +34,10 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-        <TitleCards title={"Blockbuster Movies"} />
-        <TitleCards title={"Only on Netflix"} />
-        <TitleCards title={"Upcoming"} />
-        <TitleCards title={"Top pics for you"} />
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
+        <TitleCards title={"Only on Netflix"} category={"popular"} />
+        <TitleCards title={"Upcoming"} category={"upcoming"} />
+        <TitleCards title={"Top pics for you"} category={"now_playing"} />
       </div>
       <Footer />
     </div>
