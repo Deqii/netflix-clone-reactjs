3586b4394ee8ba6cdfef83485d821b4b51f5e169
tioprayudha <tioprayudha07@gmail.com>
2024-09-25 16:06:25 +0700

    fixing typo import player.css

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,4 +1,4 @@
-import "./PLayer.css";
+import "./Player.css";
 import back_arrow_icon from "../../assets/back_arrow_icon.png";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
